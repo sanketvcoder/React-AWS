03c1863e25bc804796691bf00ba0137e2170dd47
Sanket Kumar Verma <sanketv403@gmail.com>
2025-10-25 01:05:56 +0530

    web creation stage 2

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offer from '../Components/Offers/Offer'
+import NewCollection from '../NewCollections/NewCollection'
 
 const Shop = () => {
   return (
@@ -9,6 +10,7 @@ const Shop = () => {
       <Hero/>
       <Popular/>
       <Offer/>
+      <NewCollection/>
     </div>
   )
 }
