11211fe72a85e487c1e04ada4c968962c4f43d44
Sanket Kumar Verma <sanketv403@gmail.com>
2025-10-25 14:29:50 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./Components/Navbar/Navbar"
 import Shop from "./Pages/Shop"
 import ShopCategory from "./Pages/ShopCategory"
-import Product from "./Pages/product"
+import Product from "./Pages/Product"
 import Cart from "./Pages/Cart"
 import LoginSignup from "./Pages/LoginSignup"
 
